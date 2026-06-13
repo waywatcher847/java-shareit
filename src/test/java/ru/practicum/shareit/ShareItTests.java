823afa79364268f6813c43d44ec705eb2939ec32
waywatcher847 <gmedved847@gmail.com>
2026-06-13 19:55:07 +0300

    v1.7.1

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,15 +1,13 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShareItTests {
 
-    @Test
-    @Disabled
-    void contextLoads() {
-    }
+//    @Test
+//    @Disabled
+//    void contextLoads() {
+//    }
 
 }
